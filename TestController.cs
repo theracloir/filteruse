72efb5217072b314a5f2b31eca66f02ff3fc5ddd
theracloir <148652616+theracloir@users.noreply.github.com>
2025-11-27 11:58:52 +0530

    controller

---
+++ TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApi.Filters;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [ServiceFilter(typeof(ResourceFilter))]  // Apply Resource filter at controller level
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        [ServiceFilter(typeof(TimeActionFilter))]
+        [ServiceFilter(typeof(ResultFilter))]
+        public IActionResult Hello()
+        {
+            return Ok("Hello from API");
+        }
+
+        [HttpGet("error")]
+        public IActionResult Error()
+        {
+            throw new Exception("Something went wrong!");
+        }
+    }
+}
